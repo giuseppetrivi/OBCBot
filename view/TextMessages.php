d53a043af29ff3f271bf13f998bb69b408796370
Giuseppe Trivisano <giuseppetrivisanogt@gmail.com>
2025-05-25 18:59:13 +0200

    modifica ai nomi di variabile, per renderli più brevi e ugualmente esplicativi

--- view/TextMessages.php
+++ view/TextMessages.php
@@ -29,15 +29,14 @@ final class TextMessages {
     return "➤ Invia nuovamente il nome della località di <u>partenza</u>";
   }
 
-  public static function departureLocationValid100($first_location_name) {
-    return "Hai selezionato <b>$first_location_name</b> come città di partenza";
+  public static function departureLocationValid100($location_name) {
+    return "Hai selezionato <b>$location_name</b> come città di partenza";
   }
-  public static function departureLocationValidNot100($first_location_name) {
-    return "Forse intendevi <i>$first_location_name</i>";
+  public static function departureLocationValidNot100($location_name) {
+    return "Forse intendevi <i>$location_name</i>";
   }
   public static function departureLocationNotValid($location_to_search) {
     return "La località <i>$location_to_search</i> non è tra le scelte possibili";
-
   }
 
   public static function chooseArrivalLocation() {
@@ -45,6 +44,16 @@ final class TextMessages {
   }
   public static function chooseArrivalLocationAgain() {
     return "➤ Invia nuovamente il nome della località di <u>arrivo</u>";
+  }
+
+  public static function arrivalLocationValid100($location_name) {
+    return "Hai selezionato <b>$location_name</b> come città di arrivo";
+  }
+  public static function arrivalLocationValidNot100($location_name) {
+    return "Forse intendevi <i>$location_name</i>";
+  }
+  public static function arrivalLocationNotValid($location_to_search) {
+    return "La località <i>$location_to_search</i> non è tra le scelte possibili";
   }
 
 
