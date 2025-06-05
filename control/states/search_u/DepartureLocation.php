35d0bb41164cdf53fffd37c51b3ac911fb14fefa
Giuseppe Trivisano <giuseppetrivisanogt@gmail.com>
2025-06-05 21:42:20 +0200

    prima implementazione dello stato DepartureLocation della ricerca urbana

---
+++ control/states/search_u/DepartureLocation.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace SearchU;
+
+use CustomBotName\control\AbstractState;
+use CustomBotName\entities\api_cotrap\LocationsEU;
+use CustomBotName\entities\api_cotrap\SearchEU;
+use CustomBotName\entities\api_cotrap\SearchU;
+use CustomBotName\entities\telegrambot_sdk_interface\InputTypes;
+use CustomBotName\view\Keyboards;
+use CustomBotName\view\MenuOptions;
+use CustomBotName\view\TextMessages;
+
+
+class DepartureLocation extends AbstractState {
+
+  protected array $valid_static_inputs = [
+    MenuOptions::BACK => "backProcedure",
+    MenuOptions::BACK_TO_MENU => "backProcedure"
+  ];
+
+
+  protected function validateDynamicInputs() {
+    $input_text = $this->_Bot->getInputFromChat()->getText();
+    $input_type = $this->_Bot->getInputFromChat()->getMessageType();
+
+    /* TODO */
+    $urban_location_id_regex = "/^location_\d+$/";
+    if (preg_match($urban_location_id_regex, $input_text) && $input_type==InputTypes::CALLBACK_QUERY) {
+      $this->function_to_call = "selectDepartureLocationProcedure";
+      return true;
+    }
+  }
+
+
+  /**
+   * States:
+   * SearchU\DepartureLocation -> NULL (Main)
+   */
+  protected function backProcedure() {
+    $_SearchU = new SearchU($this->_User->getUserId());
+    $_SearchU->destroySearch();
+
+    $this->_Bot->sendMessage([
+      'text' => TextMessages::mainMenu(),
+      'reply_markup' => Keyboards::getMainMenu()
+    ]);
+
+    $this->setNextState(NULL);
+  }
+
+
+  /**
+   * States:
+   * SearchU\DepartureLocation 
+   *  -> SearchU\DepartureLocation\DepartureStop
+   *     SearchU\DepartureLocation
+   */
+  protected function selectDepartureLocationProcedure() {
+    $urban_location_id = explode("_", $this->_Bot->getInputFromChat()->getText())[1];
+
+    // TODO: verificare il codice della località urbana
+
+    $_SearchU = new SearchU($this->_User->getUserId());
+    $_SearchU->setDepartureLocation($urban_location_id);
+
+    
+
+    // TODO: da cambiare
+    /*$this->_Bot->sendMessage([
+      'text' => TextMessages::chooseDepartureStop(),
+      'reply_markup' => Keyboards::getBackAndMenu()
+    ]);*/
+
+    $this->setNextState($this->appendNextState("DepartureStop"));
+  }
+
+}
+
+?>
