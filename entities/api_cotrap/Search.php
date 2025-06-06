53598b5e4a517cd802525b3d76e55a5e777a4455
Giuseppe Trivisano <giuseppetrivisanogt@gmail.com>
2025-06-06 11:25:41 +0200

    refactoring pesante
    ho reso la classe Search concreta (era astratta) e ho spostato tutti i metodi generici qui. La classe SearchU quindi è vuota, perché coincide con Search, mentre la classe SearchEU implementa il set/unset della località di arrivo, mentre il resto lo eredita da Search

--- entities/api_cotrap/Search.php
+++ entities/api_cotrap/Search.php
@@ -6,7 +6,7 @@ use CustomBotName\entities\BaseEntity;
 use DB;
 use MeekroDBException;
 
-abstract class Search extends BaseEntity {
+class Search extends BaseEntity {
 
   protected $user_idtelegram;
 
@@ -34,41 +34,94 @@ abstract class Search extends BaseEntity {
   /**
    * Get all info about search
    */
-  abstract public function getSearchInfo();
+  public function getSearchInfo() {
+    $result = 0;
+    try {
+      $result = DB::query("SELECT * FROM obc_searches WHERE user_idtelegram=%i_user_idtelegram", [
+        "user_idtelegram" => $this->getUserIdtelegram()
+      ]);
+      return $result[0];
+    } catch(MeekroDBException $e) {
+      return $result;
+    }
+  }
 
 
   /**
    * Create the record with only user_idtelegram in the search table
    */
-  abstract public function initializeSearch();
-  
+  public function initializeSearch() {
+    $result = 0;
+    try {
+      $result = DB::insert("obc_searches", [
+        "user_idtelegram" => $this->getUserIdtelegram()
+      ]);
+      return $result;
+    } catch(MeekroDBException $e) {
+      return $result;
+    }
+  }
+
   /**
    * Delete the record of search
    */
-  abstract public function destroySearch();
+  public function destroySearch() {
+    $result = 0;
+    try {
+      $result = DB::delete("obc_searches", [
+        "user_idtelegram" => $this->getUserIdtelegram()
+      ]);
+      return $result;
+    } catch(MeekroDBException $e) {
+      return $result;
+    }
+  }
 
   /**
    * Set/unset the departure location id in the initialized search record
    */
-  abstract public function setDepartureLocation($departure_location_id);
-  abstract public function unsetDepartureLocation();
+  public function setDepartureLocation($departure_location_id) {
+    return $this->updateSingleField("sea_departure_id", $departure_location_id);
+  }
+
+  public function unsetDepartureLocation() {
+    return $this->updateSingleField("sea_departure_id", NULL);
+  }
+
 
   /**
    * Set/unset the departure stop id, based on departure and arrival options
    */
-  abstract public function setDepartureStop($departure_stop_id);
-  abstract public function unsetDepartureStop();
+  public function setDepartureStop($departure_stop_id) {
+    return $this->updateSingleField("sea_departure_stop_id", $departure_stop_id);
+  }
+  
+  public function unsetDepartureStop() {
+    return $this->updateSingleField("sea_departure_stop_id", NULL);
+  }
+
 
   /**
    * Set/unset the arrival stop id, based on departure stop and arrival options
    */
-  abstract public function setArrivalStop($arrival_stop_id);
-  abstract public function unsetArrivalStop();
+  public function setArrivalStop($arrival_stop_id) {
+    return $this->updateSingleField("sea_arrival_stop_id", $arrival_stop_id);
+  }
+  
+  public function unsetArrivalStop() {
+    return $this->updateSingleField("sea_arrival_stop_id", NULL);
+  }
+
 
   /**
    * Set/unset the start datetime
    */
-  abstract public function setDatetime($datetime);
-  abstract public function unsetDatetime();
+  public function setDatetime($datetime) {
+    return $this->updateSingleField("sea_datetime", $datetime);
+  }
+  
+  public function unsetDatetime() {
+    return $this->updateSingleField("sea_datetime", NULL);
+  }
 
 }
--- entities/api_cotrap/SearchEU.php
+++ entities/api_cotrap/SearchEU.php
@@ -7,53 +7,6 @@ use MeekroDBException;
 
 class SearchEU extends Search {
 
-
-  public function getSearchInfo() {
-    $result = 0;
-    try {
-      $result = DB::query("SELECT * FROM obc_searches WHERE user_idtelegram=%i_user_idtelegram", [
-        "user_idtelegram" => $this->getUserIdtelegram()
-      ]);
-      return $result[0];
-    } catch(MeekroDBException $e) {
-      return $result;
-    }
-  }
-
-
-  public function initializeSearch() {
-    $result = 0;
-    try {
-      $result = DB::insert("obc_searches", [
-        "user_idtelegram" => $this->getUserIdtelegram()
-      ]);
-      return $result;
-    } catch(MeekroDBException $e) {
-      return $result;
-    }
-  }
-
-  public function destroySearch() {
-    $result = 0;
-    try {
-      $result = DB::delete("obc_searches", [
-        "user_idtelegram" => $this->getUserIdtelegram()
-      ]);
-      return $result;
-    } catch(MeekroDBException $e) {
-      return $result;
-    }
-  }
-
-  
-  public function setDepartureLocation($departure_location_id) {
-    return $this->updateSingleField("sea_departure_id", $departure_location_id);
-  }
-
-  public function unsetDepartureLocation() {
-    return $this->updateSingleField("sea_departure_id", NULL);
-  }
-
   
   /**
    * Set/unset the arrival location id in the initialized search record
@@ -66,31 +19,5 @@ class SearchEU extends Search {
     return $this->updateSingleField("sea_arrival_id", NULL);
   }
 
-
-  public function setDepartureStop($departure_stop_id) {
-    return $this->updateSingleField("sea_departure_stop_id", $departure_stop_id);
-  }
-  
-  public function unsetDepartureStop() {
-    return $this->updateSingleField("sea_departure_stop_id", NULL);
-  }
-
-
-  public function setArrivalStop($arrival_stop_id) {
-    return $this->updateSingleField("sea_arrival_stop_id", $arrival_stop_id);
-  }
-  
-  public function unsetArrivalStop() {
-    return $this->updateSingleField("sea_arrival_stop_id", NULL);
-  }
-
-
-  public function setDatetime($datetime) {
-    return $this->updateSingleField("sea_datetime", $datetime);
-  }
-  
-  public function unsetDatetime() {
-    return $this->updateSingleField("sea_datetime", NULL);
-  }
 
 }
