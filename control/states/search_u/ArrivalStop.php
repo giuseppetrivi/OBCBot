4bb387e44cfa59578d1640ffd4d80aea97696e2e
Giuseppe Trivisano <giuseppetrivisanogt@gmail.com>
2025-06-06 12:07:35 +0200

    implementazione completa delle procedure di selezione fermata di arrivo e data, ora e ricerca

---
+++ control/states/search_u/ArrivalStop.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace SearchU\DepartureLocation\DepartureStop;
+
+use CustomBotName\control\AbstractState;
+use CustomBotName\entities\api_cotrap\LocationsU;
+use CustomBotName\entities\api_cotrap\SearchU;
+use CustomBotName\entities\telegrambot_sdk_interface\InputTypes;
+use CustomBotName\view\InlineKeyboards;
+use CustomBotName\view\Keyboards;
+use CustomBotName\view\MenuOptions;
+use CustomBotName\view\TextMessages;
+use BackToMenuTrait;
+use CustomBotName\entities\DateTimeIT;
+
+class ArrivalStop extends AbstractState {
+
+  protected array $valid_static_inputs = [
+    MenuOptions::BACK => "backProcedure",
+    MenuOptions::BACK_TO_MENU => "backToMenuProcedure"
+  ];
+
+
+  protected function validateDynamicInputs() {
+    $input_text = $this->_Bot->getInputFromChat()->getText();
+    $input_type = $this->_Bot->getInputFromChat()->getMessageType();
+
+    /* regex to get words, eventually containing "-", as valid command. this word should be a location */
+    $stops_regex = "/\b[a-zà-öù-ýA-ZÀ-ÖÙ-Ý]+(?:\s*-\s*[a-zà-öù-ýA-ZÀ-ÖÙ-Ý]+|\s+[a-zà-öù-ýA-ZÀ-ÖÙ-Ý]+)*\b/";
+    if (preg_match($stops_regex, $input_text) && $input_type==InputTypes::MESSAGE) {
+      $this->function_to_call = "selectDepartureStopProcedure";
+      return true;
+    }
+  }
+
+
+  /**
+   * States:
+   * SearchU\DepartureLocation\DepartureStop -> SearchU\DepartureLocation
+   */
+  protected function backProcedure() {
+    $_SearchU = new SearchU($this->_User->getUserId());
+    $_SearchU->unsetDepartureStop();
+
+    $_LocationsU = new LocationsU();
+    $all_urbal_locations = $_LocationsU->getAllUrbanLocations();
+
+    $this->_Bot->sendMessage([
+      'text' => TextMessages::chooseUrbanLocation(),
+      'reply_markup' => InlineKeyboards::urbanLocationsList($all_urbal_locations)
+    ]);
+
+    $this->setNextState($this->getPreviousState());
+  }
+
+  use BackToMenuTrait;
+
+
+  /**
+   * States:
+   * SearchU\DepartureLocation\DepartureStop\ArrivalStop
+   *  -> SearchU\DepartureLocation\DepartureStop\ArrivalStop\PickDatetime
+   *     SearchU\DepartureLocation\DepartureStop\ArrivalStop
+   */
+  protected function selectDepartureStopProcedure() {
+    $stop_to_search = $this->_Bot->getInputFromChat()->getText();
+
+    $_SearchU = new SearchU($this->_User->getUserId());
+    $urban_location_id = $_SearchU->getSearchInfo()["sea_departure_id"];
+    $departure_stop_id = $_SearchU->getSearchInfo()["sea_departure_stop_id"];
+
+    $_LocationsU = new LocationsU();
+    $all_departure_stops = $_LocationsU->getAllArrivalStop($urban_location_id, $departure_stop_id);
+    $stops_info = $_LocationsU->findBestStopNameMatch($all_departure_stops, $stop_to_search);
+
+    $first_stop_code = $stops_info[0]["stop_code"];
+    $first_stop_name = $stops_info[0]["stop_name"];
+    $first_stop_similarity_perc = $stops_info[0]["similarity_perc"];
+    
+    
+    /* takes the location as valid even if there is not a perfect match */
+    if ($first_stop_similarity_perc >= LocationsU::ALMOST_MATCHED) {
+
+      if ($first_stop_similarity_perc >= LocationsU::MATCHED) {
+        $this->_Bot->sendMessage([
+          'text' => TextMessages::arrivalStopMatched($first_stop_name)
+        ]);
+      }
+      else {
+        $message_to_send = TextMessages::stopAlmostMatched($first_stop_name) .
+          "\n\n" . TextMessages::arrivalStopMatched($first_stop_name);
+        $this->_Bot->sendMessage([
+          'text' => $message_to_send
+        ]);
+      }
+
+      $_SearchU->setArrivalStop($first_stop_code);
+
+      /* datetime picker keyboard */
+      $_SelectedDatetime = new DateTimeIT(date(DateTimeIT::DATABASE_FORMAT));
+      $_SearchU->setDatetime($_SelectedDatetime->databaseFormat());
+
+      $this->_Bot->sendMessage([
+        "text" => TextMessages::selectDatetime() . "\n\n" . TextMessages::recapDatetime($_SelectedDatetime),
+        "reply_markup" => InlineKeyboards::calendar($_SelectedDatetime)
+      ]);
+
+      $this->setNextState($this->appendNextState("PickDatetime"));
+      
+    }
+    /* the match between the values ​​in the database and the value sent is not sufficient: the location must be resent */
+    else {
+      $message_to_send = TextMessages::alternativeStops($stops_info) . 
+        "\n\n" . TextMessages::chooseUrbanArrivalStopAgain();
+      $this->_Bot->sendMessage([
+        'text' => $message_to_send
+      ]);
+
+      $this->keepThisState();
+    }
+  }
+
+}
+
+?>
---
+++ control/states/search_u/PickDatetime.php
@@ -0,0 +1,251 @@
+<?php
+
+namespace SearchU\DepartureLocation\DepartureStop\ArrivalStop;
+
+use CustomBotName\control\AbstractState;
+use CustomBotName\entities\api_cotrap\ApiCotrapRequestHandler;
+use CustomBotName\entities\api_cotrap\LocationStops;
+use CustomBotName\entities\api_cotrap\SearchU;
+use CustomBotName\entities\DateTimeIT;
+use CustomBotName\entities\telegrambot_sdk_interface\InputTypes;
+use BackToMenuTrait;
+use CustomBotName\view\InlineKeyboards;
+use CustomBotName\view\MenuOptions;
+use CustomBotName\view\TextMessages;
+
+class PickDatetime extends AbstractState {
+
+  protected array $valid_static_inputs = [
+    MenuOptions::BACK => "backProcedure",
+    MenuOptions::BACK_TO_MENU => "backToMenuProcedure"
+  ];
+
+  protected function validateDynamicInputs() {
+    $input_text = $this->_Bot->getInputFromChat()->getText();
+    $input_type = $this->_Bot->getInputFromChat()->getMessageType();
+
+    /* regex to get callback_data to select day */
+    $complete_date_regex = "/^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])$/";
+    $yearmonth_regex = "/^\d{4}-(0[1-9]|1[0-2])$/";
+    $hour_regex = "/^([01]\d|2[0-3]):00$/";
+
+    if (preg_match($complete_date_regex, $input_text) && $input_type==InputTypes::CALLBACK_QUERY) {
+      $this->function_to_call = "selectDateProcedure";
+      return true;
+    }
+    /* regex to get callback_data to select month (and year) */
+    else if (preg_match($yearmonth_regex, $input_text) && $input_type==InputTypes::CALLBACK_QUERY) {
+      $this->function_to_call = "selectMonthProcedure";
+      return true;
+    }
+    /* regex to get callback_data to select hour */
+    else if (preg_match($hour_regex, $input_text) && $input_type==InputTypes::CALLBACK_QUERY) {
+      $this->function_to_call = "selectHourProcedure";
+      return true;
+    }
+    /* select the next month */
+    else if ($input_text=="next_month" && $input_type==InputTypes::CALLBACK_QUERY) {
+      $this->function_to_call = "selectNextMonthProcedure";
+      return true; 
+    }
+    /* select the previous month */
+    else if ($input_text=="previous_month" && $input_type==InputTypes::CALLBACK_QUERY) {
+      $this->function_to_call = "selectPreviousMonthProcedure";
+      return true; 
+    }
+    else if ($input_text=="search" && $input_type==InputTypes::CALLBACK_QUERY) {
+      $this->function_to_call = "searchProcedure";
+      return true; 
+    }
+    else if ( $input_text=="blank" && $input_type==InputTypes::CALLBACK_QUERY) {
+      $this->function_to_call = "emptyProcedure"; // TODO: da cambiare (procedura vuota)
+      return true; 
+    }
+
+    return false;
+  }
+
+
+  /**
+   * States:
+   * SearchU\DepartureLocation\DepartureStop\ArrivalStop\PickDatetime 
+   *  -> SearchU\DepartureLocation\DepartureStop\ArrivalStop
+   */
+  protected function backProcedure() {
+    $_SearchU = new SearchU($this->_User->getUserId());
+    $_SearchU->unsetArrivalStop();
+
+    $this->_Bot->sendMessage([
+      'text' => TextMessages::chooseUrbanArrivalStop()
+    ]);
+
+    $this->setNextState($this->getPreviousState());
+  }
+
+  /**
+   * 
+   */
+  use BackToMenuTrait;
+
+
+  /**
+   * Procedure to change the date (pressing the day button)
+   */
+  protected function selectDateProcedure() {
+    $date_selected = $this->_Bot->getInputFromChat()->getText();
+    $message_id = $this->_Bot->getWebhookUpdate()->getMessage()->getMessageId();
+
+    $_SearchU = new SearchU($this->_User->getUserId());
+    $hour = explode(" ", $_SearchU->getSearchInfo()["sea_datetime"])[1];
+
+    $_SelectedDatetime = new DateTimeIT($date_selected . " " . $hour);
+    /* check if the date is in the past (in this case sets today's date) */
+    switch ($_SelectedDatetime->isDatetimeInThePast()) {
+      case -1:
+        $_SelectedDatetime = new DateTimeIT(date(DateTimeIT::DATABASE_FORMAT));
+      case 0:
+      case 1:
+        $_SearchU->setDatetime($_SelectedDatetime->databaseFormat());
+
+        $this->_Bot->editMessageText([
+          "message_id" => $message_id,
+          "text" => TextMessages::selectDatetime() . "\n\n" . TextMessages::recapDatetime($_SelectedDatetime),
+          "reply_markup" => InlineKeyboards::calendar($_SelectedDatetime)
+        ]);
+        break;
+    }
+    
+    $this->keepThisState();
+  }
+
+
+  /**
+   * Procedure to advance the date by one month
+   */
+  protected function selectNextMonthProcedure() {
+    $message_id = $this->_Bot->getWebhookUpdate()->getMessage()->getMessageId();
+
+    $_SearchU = new SearchU($this->_User->getUserId());
+    $datetime = $_SearchU->getSearchInfo()["sea_datetime"];
+    
+    $_SelectedDatetime = new DateTimeIT($datetime);
+    $_SelectedDatetime->modify("+1 month");
+
+    $_SearchU->setDatetime($_SelectedDatetime->databaseFormat());
+
+    $this->_Bot->editMessageText([
+      "message_id" => $message_id,
+      "text" => TextMessages::selectDatetime() . "\n\n" . TextMessages::recapDatetime($_SelectedDatetime),
+      "reply_markup" => InlineKeyboards::calendar($_SelectedDatetime)
+    ]);
+    
+    $this->keepThisState();
+  }
+
+  /**
+   * Procedure to go back the date by one month (checking to not go in the past)
+   */
+  protected function selectPreviousMonthProcedure() {
+    $message_id = $this->_Bot->getWebhookUpdate()->getMessage()->getMessageId();
+
+    $_SearchU = new SearchU($this->_User->getUserId());
+    $datetime = $_SearchU->getSearchInfo()["sea_datetime"];
+    
+    $_SelectedDatetime = new DateTimeIT($datetime);
+    $_SelectedDatetime->modify("-1 month");
+    /* check if the date is in the past (in this case sets today's date) */
+    switch ($_SelectedDatetime->isDatetimeInThePast()) {
+      case -1:
+        $_SelectedDatetime = new DateTimeIT(date(DateTimeIT::DATABASE_FORMAT));
+      case 0:
+      case 1:
+        $_SearchU->setDatetime($_SelectedDatetime->databaseFormat());
+
+        $this->_Bot->editMessageText([
+          "message_id" => $message_id,
+          "text" => TextMessages::selectDatetime() . "\n\n" . TextMessages::recapDatetime($_SelectedDatetime),
+          "reply_markup" => InlineKeyboards::calendar($_SelectedDatetime)
+        ]);
+        break;
+    }
+
+    $this->keepThisState();
+  }
+
+
+  /**
+   * Procedure to set hour
+   */
+  protected function selectHourProcedure() {
+    $hour_selected = $this->_Bot->getInputFromChat()->getText();
+    $message_id = $this->_Bot->getWebhookUpdate()->getMessage()->getMessageId();
+
+    $_SearchU = new SearchU($this->_User->getUserId());
+    $date = explode(" ", $_SearchU->getSearchInfo()["sea_datetime"])[0];
+
+    $_SelectedDatetime = new DateTimeIT($date . " " . $hour_selected);
+    /* check if the date is in the past (in this case sets today's date) */
+    switch ($_SelectedDatetime->isDatetimeInThePast()) {
+      case -1:
+        $_SelectedDatetime = new DateTimeIT(date(DateTimeIT::DATABASE_FORMAT));
+      case 0:
+      case 1:
+        $_SearchU->setDatetime($_SelectedDatetime->databaseFormat());
+
+        $this->_Bot->editMessageText([
+          "message_id" => $message_id,
+          "text" => TextMessages::selectDatetime() . "\n\n" . TextMessages::recapDatetime($_SelectedDatetime),
+          "reply_markup" => InlineKeyboards::calendar($_SelectedDatetime)
+        ]);
+        break;
+    }
+    
+    $this->keepThisState();
+  }
+
+
+  /**
+   * Start the search of timetables, taking all data previously given
+   */
+  public function searchProcedure() {
+    $_SearchU = new SearchU($this->_User->getUserId());
+    $search_info = $_SearchU->getSearchInfo();
+    
+    $_Datetime = new DateTimeIT($search_info["sea_datetime"]);
+    $formatted_date = $_Datetime->getApiFormattedDate();
+    $formatted_time = $_Datetime->getApiFormattedTime();
+
+    $_ApiCotrap = new ApiCotrapRequestHandler();
+    $request_result_data = $_ApiCotrap->get("search_u", [], [
+      "idLocalita" => $search_info["sea_departure_id"],
+      "idPoloPartenza" => $search_info["sea_departure_stop_id"],
+      "idPoloArrivo" => $search_info["sea_arrival_stop_id"],
+      "dataPartenza" => $formatted_date,
+      "oraPartenza" => $formatted_time,
+      "pagina" => 1,
+    ]);
+
+    $search_results = $request_result_data["result"]["itinerariTrovati"];
+
+    if (empty($search_results)) {
+      $this->_Bot->sendMessage([
+        'text' => TextMessages::noSearchResults()
+      ]);
+
+      $this->keepThisState();
+    }
+    else {
+      $this->_Bot->sendMessage([
+        'text' => TextMessages::showSearchResults($search_results, $_Datetime),
+        'reply_markup' => InlineKeyboards::websiteResultsLink($request_result_data["url"]),
+        'disable_web_page_preview' => true
+      ]);
+    }
+
+    $this->keepThisState(); // TODO: da cambiare
+  }
+
+
+}
+
+?>
