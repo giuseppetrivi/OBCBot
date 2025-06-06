d0517566fa8ee509555a511f5e1f57a21d42be4e
Giuseppe Trivisano <giuseppetrivisanogt@gmail.com>
2025-06-06 19:16:02 +0200

    aggiunta della funzione per ottenere tutte le informazioni utili della ricerca

--- entities/api_cotrap/SearchEU.php
+++ entities/api_cotrap/SearchEU.php
@@ -2,6 +2,8 @@
 
 namespace CustomBotName\entities\api_cotrap;
 
+use DB;
+
 /**
  * Specific class when in searching eu procedure
  */
@@ -18,5 +20,26 @@ class SearchEU extends Search {
     return $this->updateSingleField("sea_arrival_id", NULL);
   }
 
+
+  /**
+   * 
+   */
+  public function getSpecificSearchInfo() {
+    $result_departure = DB::query("SELECT eu.denominazione as comune, pl.denominazione as fermata, pl.latitudine, pl.longitudine, az.denominazione as azienda, sea.sea_datetime 
+      FROM obc_searches as sea JOIN cotrap_localita_eu as eu JOIN cotrap_polilocalita as pl JOIN cotrap_aziende as az 
+      WHERE sea.sea_departure_id=eu.codice AND pl.idAzienda=az.id AND sea.sea_departure_stop_id=pl.id AND sea.user_idtelegram=%i_user_idtelegram", [
+        "user_idtelegram" => $this->getUserIdtelegram()
+      ])[0];
+    $result_arrival = DB::query("SELECT eu.denominazione as comune, pl.denominazione as fermata, pl.latitudine, pl.longitudine
+      FROM obc_searches as sea JOIN cotrap_localita_eu as eu JOIN cotrap_polilocalita as pl 
+      WHERE sea.sea_arrival_id=eu.codice AND sea.sea_arrival_stop_id=pl.id AND sea.user_idtelegram=%i_user_idtelegram", [
+        "user_idtelegram" => $this->getUserIdtelegram()
+      ])[0];
+    
+    return [
+      "departure" => $result_departure,
+      "arrival" => $result_arrival
+    ];
+  }
 
 }
