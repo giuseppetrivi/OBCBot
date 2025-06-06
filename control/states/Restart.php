1fefe65b6a888a7281b295ccc820180947343bec
Giuseppe Trivisano <giuseppetrivisanogt@gmail.com>
2025-06-06 15:48:39 +0200

    modifiche banali (commenti e classe Search)

--- control/states/Restart.php
+++ control/states/Restart.php
@@ -1,14 +1,12 @@
 <?php
 
 use CustomBotName\control\AbstractState;
-use CustomBotName\entities\api_cotrap\SearchEU;
+use CustomBotName\entities\api_cotrap\Search;
 use CustomBotName\view\MenuOptions;
 use CustomBotName\view\Keyboards;
 use CustomBotName\view\TextMessages;
 
-/**
- * 
- */
+
 class Restart extends AbstractState {
 
   protected array $valid_static_inputs = [
@@ -17,11 +15,11 @@ class Restart extends AbstractState {
 
 
   /**
-   * Method to handle the behavior after static input view\MenuOptions::COMMAND_RESTART
+   * Procedure to handle the restart command (/restart), which has a priority on ony other command
    */
   protected function restartProcedure() {
-    $_SearchEU = new SearchEU($this->_User->getUserId());
-    $_SearchEU->destroySearch();
+    $_Search = new Search($this->_User->getUserId());
+    $_Search->destroySearch();
 
     $this->_Bot->sendMessage([
       'text' => TextMessages::mainMenuFromRestart(),
